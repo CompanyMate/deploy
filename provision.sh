e219d109e228817dcc8a8854a5e6e061e1f4fe92
Derek Bourgeois <derek@ibourgeois.com>
2015-08-31 11:36:52 -0400

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+read -e -p "Enter your Domain Name: " DOMAIN
+read -e -p "Enter your Application Name: " APPNAME
 read -e -p "Enter your Username: " NAME
+read -e -p "Enter your Password: " PASSWORD
 read -e -p "Enter your SSH Key: " SSH
 
 # Update Server
@@ -23,7 +26,7 @@ apt-get install -y build-essential libssl-dev nano wget curl
 
 # Configure Timezone
 -ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
-apt-get install ntp
+apt-get install -y ntp
 
 # Create Swapfile
 fallocate -l 4G /swapfile
@@ -45,6 +48,90 @@ sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
 service ssh restart
 
 # Install and Configure Nginx
-apt-get install nginx
-sed -i "s/user www-data;/user $USER;/" /etc/nginx.nginx.conf
+apt-get install -y nginx
+sed -i "s/user www-data;/user $USER;/" /etc/nginx/nginx.conf
+mkdir -p /var/www/html
+cat <<EOF >> /etc/nginx/sites-available/default
+server {
+  listen 80 default_server;
+  listen [::]:80 default_server ipv6only=on;
+  root /var/www/html/$APPNAME/public;
+  index index.php index.html index.htm;
+  server_name localhost;
+  
+  location / {
+    try_files \$uri \$uri/ =404;
+  }
+  
+  error_page 404 /404.html;
+  error_page 500 502 503 504 /50x.html;
+  
+  location = /50x.html {
+    root /var/www/html/$APPNAME/public;
+  }
+  
+  location ~ \\.php\$ {
+    try_files \$uri =404;
+    fastcgi_split_path_info ^(.+\\.php)(/.+)\$;
+    fastcgi_pass unix:/var/run/php5-fpm.sock; 
+    fastcgi_index index.php;
+    include fastcgi.conf;
+  }
+}
+EOF
 service nginx restart
+
+# Install and Configure PHP-FPM
+apt-get install -y php5-fpm php5-curl php5-json php5-cgi php5-cli php5-redis php5-common php5-mcrypt \
+  php5-memcached php5-sqlite php5-mysql php5-oauth php5-imagick php5-imap php5-dev php5-pear \
+  php5-mysqlnd php5-apcu php5-gd php5-gmp php5-xdebug
+sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
+ln -s /etc/php5/conf.d/mcrypt.ini /etc/php5/mods-available
+php5enmod mcrypt
+service nginx restart
+service php5 restart
+
+# Install and Configure Composer
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+printf "\nPATH=\"/home/$USER/.composer/vendor/bin:\$PATH\"\n" | tee -a /home/$USER/.profile
+
+# Install and Configure MySQL
+
+
+# Install and Configure Node.JS and NPM
+curl -sL https://deb.nodesource.com/setup | sudo bash -
+apt-get install -y nodejs
+
+# Install and Configure Grunt
+npm install -g grunt-cli
+
+# Install and Configure Gulp
+npm install -g gulp
+
+# Install and Configure Bower
+npm install -g bower
+
+# Install and Configure Redis
+apt-get install -y redis-server redis-tools
+
+# Install and Configure Git
+apt-get install -y git
+
+# Install and Configure Haraka
+
+
+# Install and Configure Laravel
+composer global require "laravel/installer=~1.1"
+
+# Install and Configure Lumen
+composer global require "laravel/lumen-installer=~1.0"
+
+# Install and Configure Envoy
+composer global require "laravel/envoy=~1.0"
+
+# Install and Configure Application
+cd /var/www/html
+laravel new $APPNAME
+cd /var/www/html/$APPNAME
+npm install
