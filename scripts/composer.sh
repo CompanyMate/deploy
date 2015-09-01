9957137ff6f184b23f1c48fef6f780d764542f7d
Derek Bourgeois <derek@ibourgeois.com>
2015-09-01 04:03:48 -0400

    Update composer.sh

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+echo "================================================================================"
+echo " Installing Composer "
+echo "================================================================================"
+
+curl -sS https://getcomposer.org/installer | php
+
+echo "================================================================================"
+echo " Configuring Composer "
+echo "================================================================================"
+
+sudo mv composer.phar /usr/local/bin/composer
+sudo echo "alias composer='sudo /usr/local/bin/composer'" >> ~/.bashrc
