d87e3d36301ff21e248fa66914edaaad0df7ac0e
Derek Bourgeois <derek@ibourgeois.com>
2015-09-01 02:38:27 -0400

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -10,20 +10,8 @@ read -e -p "Enter your SSH Key: " SSH
 apt-get update
 apt-get upgrade -y
 
-# Create Group
-addgroup $NAME
-
-# Create User
-sudo su -c "useradd $NAME -s /bin/bash -m -g sudo -G $NAME"
-mkdir -p /home/$NAME/.ssh
-su $NAME <<'EOF'
-chmod 700 /home/$NAME/.ssh
-EOF
-echo $SSH >> /home/$NAME/.ssh/authorized_keys
-echo "$NAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
 # Install Dependencies
-apt-get install -y build-essential libssl-dev nano wget curl memcached
+apt-get install -y build-essential libssl-dev wget memcached
 
 # Configure Timezone
 -ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
@@ -42,16 +30,39 @@ echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
 ufw allow 22/tcp
 ufw allow 80/tcp
 ufw allow 443/tcp
-ufw enable -y
+ufw -y enable
+
+# Configure SSH
+sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
+service ssh restart
+
+# Create User
+sudo su -c "useradd $NAME -s /bin/bash -m -g sudo"
+mkdir -p /home/$NAME/.ssh
+su $NAME <<'EOF'
+chmod 700 /home/$NAME/.ssh
+EOF
+echo $SSH >> /home/$NAME/.ssh/authorized_keys
+echo "$NAME ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+
+# Configure Fail2Ban
+apt-get install -y fail2ban
+
+# Run Installation as New User
+sudo su $NAME <<'EOS'
+cd ~/
 
 # Install and Configure Nginx
-apt-get install -y nginx
-mkdir -p /var/www/html
-cat <<EOF > /etc/nginx/sites-available/default
+sudo apt-get install -y nginx
+sudo mkdir -p /var/www/html/$APPNAME/current/public
+sudo cat <<EOF > /var/www/html/$APPNAME/current/public/index.php
+<?php echo ucase($APPNAME); ?>
+EOF
+sudo cat <<EOF > /etc/nginx/sites-available/default
 server {
   listen 80 default_server;
   listen [::]:80 default_server ipv6only=on;
-  root /var/www/html/$APPNAME/public;
+  root /var/www/html/$APPNAME/current/public;
   index index.php index.html index.htm;
   server_name localhost;
   
@@ -63,7 +74,7 @@ server {
   error_page 500 502 503 504 /50x.html;
   
   location = /50x.html {
-    root /var/www/html/$APPNAME/public;
+    root /var/www/html/$APPNAME/current/public;
   }
   
   location ~ \\.php\$ {
@@ -75,79 +86,60 @@ server {
   }
 }
 EOF
-service nginx restart
+sudo service nginx restart
 
 # Install and Configure PHP-FPM
-apt-get install -y php5-fpm php5-curl php5-json php5-cgi php5-cli php5-redis php5-common php5-mcrypt \
+sudo apt-get install -y php5-fpm php5-curl php5-json php5-cgi php5-cli php5-redis php5-common php5-mcrypt \
   php5-memcached php5-sqlite php5-oauth php5-imagick php5-imap php5-dev php-pear php5-mysqlnd php5-apcu \
   php5-gd php5-gmp php5-xdebug
-sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
-ln -s /etc/php5/conf.d/mcrypt.ini /etc/php5/mods-available
-php5enmod mcrypt
-service nginx restart
-service php5 restart
+sudo sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php5/fpm/php.ini
+sudo ln -s /etc/php5/conf.d/mcrypt.ini /etc/php5/mods-available
+sudo php5enmod mcrypt
+sudo service nginx restart
+sudo service php5 restart
 
 # Install and Configure Composer
 curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
-printf "\nPATH=\"/home/$NAME/.composer/vendor/bin:\$PATH\"\n" | tee -a /home/$NAME/.profile
-echo "alias composer='/user/local/bin/composer'" >> /home/$NAME/.bashrc
+sudo mv composer.phar /usr/local/bin/composer
+sudo printf "\nPATH=\"/home/$NAME/.composer/vendor/bin:\$PATH\"\n" | tee -a /home/$NAME/.profile
+sudo echo "alias composer='/usr/local/bin/composer'" >> /home/$NAME/.bashrc
 
 # Install and Configure MySQL
 
 
 # Install and Configure Node.JS and NPM
 curl -sL https://deb.nodesource.com/setup | sudo bash -
-apt-get install -y nodejs
-sudo chown -R /root/
+sudo apt-get install -y nodejs
 
 # Install and Configure Grunt
-su $NAME <<'EOF'
 sudo npm install -g grunt-cli
-EOF
 
 # Install and Configure Gulp
-su $NAME <<'EOF'
 sudo npm install -g gulp
-EOF
 
 # Install and Configure Bower
-su $NAME <<'EOF'
 sudo npm install -g bower
-EOF
 
 # Install and Configure Redis
-apt-get install -y redis-server redis-tools
+sudo apt-get install -y redis-server redis-tools
 
 # Install and Configure Git
-apt-get install -y git
+sudo apt-get install -y git git-core
 
 # Install and Configure Haraka
-su $NAME <<'EOF'
 sudo npm install -g Haraka
-EOF
-su $NAME <<'EOF'
-haraka -i /usr/share/mail
-EOF
+# haraka -i /usr/share/mail
 
 # Install and Configure Laravel
-su $NAME <<'EOF'
-/usr/local/bin/composer global require "laravel/installer=~1.1"
-EOF
-echo "alias laravel='/home/$NAME/.composer/bin/laravel/laravel'" >> /home/$NAME/.bashrc
+sudo composer global require "laravel/installer=~1.1"
+sudo echo "alias laravel='/home/$NAME/.composer/vendor/laravel/installer/laravel'" >> /home/$NAME/.bashrc
 
 # Install and Configure Lumen
-su $NAME <<'EOF'
-/usr/local/bin/composer global require "laravel/lumen-installer=~1.0"
-EOF
-echo "alias lumen='/home/$NAME/.composer/bin/lumen/lumen'" >> /home/$NAME/.bashrc
+sudo composer global require "laravel/lumen-installer=~1.0"
+sudo echo "alias lumen='/home/$NAME/.composer/vendor/laravel/lumen/lumen'" >> /home/$NAME/.bashrc
 
 # Install and Configure Envoy
-su $NAME <<'EOF'
-/usr/local/bin/composer global require "laravel/envoy=~1.0"
-EOF
-echo "alias envoy='/home/$NAME/.composer/bin/envoy/envoy'" >> /home/$NAME/.bashrc
+sudo /usr/local/bin/composer global require "laravel/envoy=~1.0"
+sudo echo "alias envoy='/home/$NAME/.composer/vendor/laravel/envoy/envoy'" >> /home/$NAME/.bashrc
 
-# Configure SSH
-sed -i -e '/^PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
-service ssh restart
+EOS
